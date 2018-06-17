0f21bf8fbd71475deacabddbf7a899f824d18972
DzianisH <gavrilovetsden@gmail.com>
2018-06-17 12:47:24 +0300

    Add content checker

--- fill_db_runner.py
+++ fill_db_runner.py
@@ -9,8 +9,8 @@ client = MongoClient()
 db = client['glove_db']
 
 dimensionality = 25
-collection = db['glove_twitter_{}d'.format(dimensionality)]
-print(collection)
+glove = db['glove_twitter_{}d'.format(dimensionality)]
+print(glove)
 
 
 def create_doc(bulks: pd.DataFrame, idx: int) -> Dict[str, Union[List[float], str]]:
@@ -20,19 +20,28 @@ def create_doc(bulks: pd.DataFrame, idx: int) -> Dict[str, Union[List[float], st
     }
 
 
-df = pd.read_csv('data/glove.twitter.27B.25d.txt', delim_whitespace=True, quoting=_csv.QUOTE_NONE)
+def populate_database(glove_source, glove_collection):
+    df = pd.read_csv(glove_source, delim_whitespace=True, quoting=_csv.QUOTE_NONE)
+    bulk_size = 32000
+    words_number = df.shape[0]
+    print("Going to convert", words_number, "items by", bulk_size, "in the batch")
+    start_time = time.time()
+    for first in range(0, words_number, bulk_size):
+        bulks = df[first:(first + bulk_size)]
+        docs = [create_doc(bulks, i + first) for i in range(bulks.shape[0])]
 
-bulk_size = 32000
-words_number = df.shape[0]
-print("Going to convert", words_number, "items by", bulk_size, "in the batch")
-start_time = time.time()
-for first in range(0, words_number, bulk_size):
-    bulks = df[first:(first + bulk_size)]
-    docs = [create_doc(bulks, i + first) for i in range(bulks.shape[0])]
+        glove_collection.insert_many(docs)
 
-    collection.insert_many(docs)
+        done = float(first + len(docs)) / words_number
+        time_delay = time.time() - start_time
+        print("Done: {:.2f}%, spent: {:.2f}sec, ETA: {:.2f}sec"
+              .format(done * 100, time_delay, time_delay * (1 - done) / done))
+    print('Indexing..')
+    glove.create_index("word")
 
-    done = float(first + len(docs)) / words_number
-    time_delay = time.time() - start_time
-    print("Done: {:.2f}%, spent: {:.2f}sec, ETA: {:.2f}sec"
-          .format(done * 100, time_delay, time_delay * (1 - done) / done))
+
+count = glove.count()
+if count < 1000000:
+    populate_database('data/glove.twitter.27B.25d.txt', glove)
+else:
+    print("wont populate database, as it already contains", count, "items")
